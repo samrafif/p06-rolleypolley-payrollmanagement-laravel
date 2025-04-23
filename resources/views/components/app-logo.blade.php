ca355f62401a1cd7e8597e354b2c6e081ec0d841
Samwoof <abrisam.rm@gmail.com>
2025-04-23 11:18:49 +0700

    Updated Logo

--- resources/views/components/app-logo.blade.php
+++ resources/views/components/app-logo.blade.php
@@ -1,6 +1,8 @@
 <div class="flex aspect-square size-8 items-center justify-center rounded-md bg-accent-content text-accent-foreground">
     <x-app-logo-icon class="size-5 fill-current text-white dark:text-black" />
 </div>
-<div class="ms-1 grid flex-1 text-start text-sm">
-    <span class="mb-0.5 truncate leading-none font-semibold">Laravel Starter Kit</span>
+<div class="ms-1 grid flex-1 text-start text-sm gap-1">
+    <span class="mb-0.5 truncate leading-none font-semibold">RolleyPolley</span>
+    {{-- TODO: INFO TAKE FROM DB --}}
+    <span class="mb-0.5 truncate leading-none">COMPANY_NAME</span>
 </div>
